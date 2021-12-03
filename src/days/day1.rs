9f1e3bf067f9e38c435a9e96a2ab3515e256f802
angela_brite <angela.brite@protonmail.com>
2021-12-03 12:58:08 -0700

    day 1 part 1 done

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,5 +1,11 @@
 use crate::common::lines_from_file;
 
 pub fn run() {
-    let input = lines_from_file("data/day1_input.txt");
+    let input = lines_from_file("data/day1_input.txt").unwrap();
+    let depths: Vec<u32> = input.iter().map(|line| line.parse::<u32>().unwrap()).collect();
+    let depth_increases = depths
+        .windows(2)
+        .filter(|depth| depth[1] > depth[0])
+        .count();
+    println!("Depth changes: {}", depth_increases)
 }
