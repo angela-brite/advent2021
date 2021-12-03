cdf501215c1d80a60d02091a72f7d2c5762bf72a
angela_brite <angela.brite@protonmail.com>
2021-12-03 13:17:54 -0700

    Rudimentary part selection

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,6 +1,14 @@
 use crate::common::lines_from_file;
 
-pub fn run() {
+pub fn run(part: u32) {
+    match part {
+        1 => part_one(),
+        2 => part_two(),
+        _ => eprintln!("That part isn't available!")
+    }
+}
+
+fn part_one() {
     let input = lines_from_file("data/day1_input.txt").unwrap();
     let depths: Vec<u32> = input.iter().map(|line| line.parse::<u32>().unwrap()).collect();
     let depth_increases = depths
@@ -8,4 +16,8 @@ pub fn run() {
         .filter(|depth| depth[1] > depth[0])
         .count();
     println!("Depth changes: {}", depth_increases)
+}
+
+fn part_two() {
+    todo!()
 }
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ use crate::days::*;
 
 fn main() -> Result<(), Error>{
     let args: Vec<String> = env::args().collect();
-    let selection = args[1].parse::<i32>()?;
+    let selection = args[1].parse::<u32>()?;
+    let part = args.get(2).unwrap_or(&("1".to_string())).parse::<u32>()?;
 
     match selection {
-        1 => day1::run(),
+        1 => day1::run(part),
         _ => println!("Unknown day provided!"),
     };
     Ok(())
