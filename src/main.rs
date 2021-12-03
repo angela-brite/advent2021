399d4d85387d971e0bf015a4cc9e2f6f6ad94b35
angela_brite <angela.brite@protonmail.com>
2021-12-03 12:11:44 -0700

    Initial setup

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+use anyhow::Error;
+mod days;
+use crate::days::*;
+
+fn main() -> Result<(), Error>{
+    let args: Vec<String> = env::args().collect();
+    let selection = args[1].parse::<i32>()?;
+
+    match selection {
+        1 => day1::run(),
+        _ => println!("Unknown day provided!"),
+    };
+    Ok(())
+}
